c1aee2491be1f26ba64951a13169046953fe81ee
Tobias Erbshäußer <tobias.erbshaeusser@gmail.com>
2020-07-30 10:21:10 +0200

    Replaced private declaration start __ with P_ to prevent clashes with reserved names.

--- include/nt/assert.h
+++ include/nt/assert.h
@@ -6,12 +6,12 @@
 #include <stdlib.h>
 
 #define nt_assert(condition, ...)                                              \
-  __nt_assert(condition, NT_GET_ARG0_OR_DEFAULT(NULL, __VA_ARGS__), NT_LOCATION)
+  P_nt_assert(condition, NT_GET_ARG0_OR_DEFAULT(NULL, __VA_ARGS__), NT_LOCATION)
 
 #ifdef NT_DISABLE_ASSERTS
-#define __nt_assert(condition, message, location)
+#define P_nt_assert(condition, message, location)
 #else
-#define __nt_assert(condition, message, location)                              \
+#define P_nt_assert(condition, message, location)                              \
   if (!(condition)) {                                                          \
     nt_panic("Assertion failed at %s\n\n%s\n\n%s", location,                   \
              NT_STRINGIFY(condition), message ? message : "");                 \
