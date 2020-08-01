249531d63570cb4af00789b09b9f7589cad65cb1
Tobias Erbshäußer <tobias.erbshaeusser@gmail.com>
2020-08-01 12:33:18 +0200

    Extended circular buffers to support static and heap memory.

--- include/nt/circular.h
+++ include/nt/circular.h
@@ -4,16 +4,19 @@
 #include "nt/mem.h"
 #include <stdlib.h>
 
-#define NT_CIRC_BUF(name, item_type)                                           \
+#define NT_CIRC_BUF(...)                                                       \
+  NT_GET_ARG3(__VA_ARGS__, P_NT_CIRC_BUF_FX, P_NT_CIRC_BUF_DYN)(__VA_ARGS__)
+
+#define P_NT_CIRC_BUF_DYN(name, item_type)                                     \
   typedef struct {                                                             \
-    item_type *P_items;                                                        \
-    size_t P_capacity;                                                         \
     size_t P_first;                                                            \
     size_t P_count;                                                            \
+    size_t P_capacity;                                                         \
+    item_type *P_items;                                                        \
   } name##_t;                                                                  \
                                                                                \
   static inline name##_t name##_new(size_t capacity) {                         \
-    return (name##_t){nt_alloc(item_type, capacity), capacity, 0, 0};          \
+    return (name##_t){0, 0, capacity, nt_alloc(item_type, capacity)};          \
   }                                                                            \
                                                                                \
   static inline void name##_free(name##_t *self) { nt_free(self->P_items); }   \
@@ -27,28 +30,77 @@
   }                                                                            \
                                                                                \
   static inline void name##_push(name##_t *self, item_type item) {             \
-    P_nt_circ_buf_push(self, &item, sizeof(item_type));                        \
+    P_nt_circ_buf_dyn_push(self, &item, sizeof(item_type));                    \
   }                                                                            \
                                                                                \
   static inline void name##_push_ref(name##_t *self, item_type const *item) {  \
-    P_nt_circ_buf_push(self, item, sizeof(item_type));                         \
+    P_nt_circ_buf_dyn_push(self, item, sizeof(item_type));                     \
   }                                                                            \
                                                                                \
   static inline void name##_pop(name##_t *self, item_type *item) {             \
-    P_nt_circ_buf_pop(self, item, sizeof(item_type));                          \
+    P_nt_circ_buf_dyn_pop(self, item, sizeof(item_type));                      \
   }                                                                            \
                                                                                \
   static inline item_type const *name##_get(name##_t const *self,              \
                                             size_t index) {                    \
-    return P_nt_circ_buf_get(self, index, sizeof(item_type));                  \
+    return P_nt_circ_buf_dyn_get(self, index, sizeof(item_type));              \
   }                                                                            \
                                                                                \
   static inline void name##_clear(name##_t *self) { self->P_count = 0; }
 
-void P_nt_circ_buf_push(void *self, void const *item, size_t item_size);
+void P_nt_circ_buf_dyn_push(void *self, void const *item, size_t item_size);
 
-void P_nt_circ_buf_pop(void *self, void *item, size_t item_size);
+void P_nt_circ_buf_dyn_pop(void *self, void *item, size_t item_size);
 
-void const *P_nt_circ_buf_get(void const *self, size_t index, size_t item_size);
+void const *P_nt_circ_buf_dyn_get(void const *self, size_t index,
+                                  size_t item_size);
+
+#define P_NT_CIRC_BUF_FX(name, item_type, buf_capacity)                        \
+  typedef struct {                                                             \
+    size_t P_first;                                                            \
+    size_t P_count;                                                            \
+    item_type P_items[buf_capacity];                                           \
+  } name##_t;                                                                  \
+                                                                               \
+  static inline name##_t name##_new() { return (name##_t){0}; }                \
+                                                                               \
+  static inline void name##_free(name##_t *self) { (void)self; }               \
+                                                                               \
+  static inline size_t name##_capacity(name##_t const *self) {                 \
+    (void)self;                                                                \
+    return buf_capacity;                                                       \
+  }                                                                            \
+                                                                               \
+  static inline size_t name##_count(name##_t const *self) {                    \
+    return self->P_count;                                                      \
+  }                                                                            \
+                                                                               \
+  static inline void name##_push(name##_t *self, item_type item) {             \
+    P_nt_circ_buf_fx_push(self, buf_capacity, &item, sizeof(item_type));       \
+  }                                                                            \
+                                                                               \
+  static inline void name##_push_ref(name##_t *self, item_type const *item) {  \
+    P_nt_circ_buf_fx_push(self, buf_capacity, item, sizeof(item_type));        \
+  }                                                                            \
+                                                                               \
+  static inline void name##_pop(name##_t *self, item_type *item) {             \
+    P_nt_circ_buf_fx_pop(self, buf_capacity, item, sizeof(item_type));         \
+  }                                                                            \
+                                                                               \
+  static inline item_type const *name##_get(name##_t const *self,              \
+                                            size_t index) {                    \
+    return P_nt_circ_buf_fx_get(self, buf_capacity, index, sizeof(item_type)); \
+  }                                                                            \
+                                                                               \
+  static inline void name##_clear(name##_t *self) { self->P_count = 0; }
+
+void P_nt_circ_buf_fx_push(void *self, size_t capacity, void const *item,
+                           size_t item_size);
+
+void P_nt_circ_buf_fx_pop(void *self, size_t capacity, void *item,
+                          size_t item_size);
+
+void const *P_nt_circ_buf_fx_get(void const *self, size_t capacity,
+                                 size_t index, size_t item_size);
 
 #endif // NTUTILS_CIRCULAR_H
--- source/circular.c
+++ source/circular.c
@@ -8,7 +8,7 @@ static void *get_item_ptr(void_buff_t *self, size_t index, size_t item_size) {
   return self->P_items + (self->P_first + index) % self->P_capacity * item_size;
 }
 
-void P_nt_circ_buf_push(void *self, void const *item, size_t item_size) {
+void P_nt_circ_buf_dyn_push(void *self, void const *item, size_t item_size) {
   void_buff_t *self2 = self;
 
   nt_assert(self2->P_count < self2->P_capacity,
@@ -20,7 +20,7 @@ void P_nt_circ_buf_push(void *self, void const *item, size_t item_size) {
   ++self2->P_count;
 }
 
-void P_nt_circ_buf_pop(void *self, void *item, size_t item_size) {
+void P_nt_circ_buf_dyn_pop(void *self, void *item, size_t item_size) {
   void_buff_t *self2 = self;
 
   nt_assert(self2->P_count > 0, "Cannot pop from an empty circular buffer");
@@ -34,8 +34,8 @@ void P_nt_circ_buf_pop(void *self, void *item, size_t item_size) {
   --self2->P_count;
 }
 
-void const *P_nt_circ_buf_get(void const *self, size_t index,
-                              size_t item_size) {
+void const *P_nt_circ_buf_dyn_get(void const *self, size_t index,
+                                  size_t item_size) {
   void_buff_t const *self2 = self;
 
   nt_assert(
@@ -44,4 +44,50 @@ void const *P_nt_circ_buf_get(void const *self, size_t index,
       index, self2->P_count);
 
   return get_item_ptr((void_buff_t *)self2, index, item_size);
+}
+
+NT_CIRC_BUF(void_buff_fx, char, 1)
+
+static void *get_item_ptr_fx(void_buff_fx_t *self, size_t capacity,
+                             size_t index, size_t item_size) {
+  return self->P_items + (self->P_first + index) % capacity * item_size;
+}
+
+void P_nt_circ_buf_fx_push(void *self, size_t capacity, void const *item,
+                           size_t item_size) {
+  void_buff_fx_t *self2 = self;
+
+  nt_assert(self2->P_count < capacity, "Cannot push on a full circular buffer");
+
+  void *ptr = get_item_ptr_fx(self2, capacity, self2->P_count, item_size);
+  memcpy(ptr, item, item_size);
+
+  ++self2->P_count;
+}
+
+void P_nt_circ_buf_fx_pop(void *self, size_t capacity, void *item,
+                          size_t item_size) {
+  void_buff_fx_t *self2 = self;
+
+  nt_assert(self2->P_count > 0, "Cannot pop from an empty circular buffer");
+
+  if (item) {
+    void *ptr = get_item_ptr_fx(self2, capacity, 0, item_size);
+    memcpy(item, ptr, item_size);
+  }
+
+  self2->P_first = (self2->P_first + 1) % capacity;
+  --self2->P_count;
+}
+
+void const *P_nt_circ_buf_fx_get(void const *self, size_t capacity,
+                                 size_t index, size_t item_size) {
+  void_buff_fx_t const *self2 = self;
+
+  nt_assert(
+      index < self2->P_count,
+      "Index %zu is out of range of a circular buffer containing %zu items",
+      index, self2->P_count);
+
+  return get_item_ptr_fx((void_buff_fx_t *)self2, capacity, index, item_size);
 }
