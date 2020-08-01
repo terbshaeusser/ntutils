1f6e1e1d43d231138b5655ff9f0aa0149ba631fc
Tobias Erbshäußer <tobias.erbshaeusser@gmail.com>
2020-08-01 12:43:19 +0200

    Changed comparator return type to enum.

--- include/nt/compare.h
+++ include/nt/compare.h
@@ -1,8 +1,12 @@
 #ifndef NTUTILS_COMPARE_H
 #define NTUTILS_COMPARE_H
 
-enum { NT_COMPARE_ASC = -1, NT_COMPARE_EQUAL, NT_COMPARE_DESC };
+typedef enum {
+  NT_COMPARE_ASC = -1,
+  NT_COMPARE_EQUAL,
+  NT_COMPARE_DESC
+} nt_comparison_result_t;
 
-#define nt_comparator_t(name, type) int (*name)(type, type)
+#define nt_comparator_t(name, type) nt_comparison_result_t (*name)(type, type)
 
 #endif // NTUTILS_COMPARE_H
