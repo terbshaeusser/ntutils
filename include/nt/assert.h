9711b9c64cc5713b12bd951c48e4d88c985e7e44
Tobias Erbshäußer <tobias.erbshaeusser@gmail.com>
2020-08-02 19:00:18 +0200

    Changed nt_assert so that format arguments can be passed.

--- include/nt/assert.h
+++ include/nt/assert.h
@@ -6,15 +6,19 @@
 #include <stdlib.h>
 
 #define nt_assert(condition, ...)                                              \
-  P_nt_assert(condition, NT_GET_ARG0_OR_DEFAULT(NULL, __VA_ARGS__), NT_LOCATION)
+  P_nt_assert(condition, NT_GET_ARG0_OR_DEFAULT("", __VA_ARGS__),              \
+              NT_GET_ARG1_TO_N(__VA_ARGS__))
+
+#define P_nt_assert(condition, message, ...)                                   \
+  P_nt_assert2(condition, message, NT_LOCATION __VA_OPT__(, ) __VA_ARGS__)
 
 #ifdef NT_DISABLE_ASSERTS
-#define P_nt_assert(condition, message, location)
+#define P_nt_assert2(condition, message, location)
 #else
-#define P_nt_assert(condition, message, location)                              \
+#define P_nt_assert2(condition, message, location, ...)                        \
   if (!(condition))                                                            \
-  nt_panic("Assertion failed at %s\n\n%s\n\n%s", location,                     \
-           NT_STRINGIFY(condition), message ? message : "")
+  nt_panic("Assertion failed at " location "\n\n%s\n\n" message,               \
+           NT_STRINGIFY(condition) __VA_OPT__(, ) __VA_ARGS__)
 #endif
 
 #endif // NTUTILS_ASSERT_H
