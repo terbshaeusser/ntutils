e0a093d0ca50e0adbe5fd4710f781f53577a5966
Tobias Erbshäußer <tobias.erbshaeusser@gmail.com>
2020-08-04 07:52:23 +0200

    Moved item size for circular buffers and lists into the struct.

--- source/circular.c
+++ source/circular.c
@@ -4,38 +4,38 @@
 
 NT_CIRC_BUF(void_buff, char)
 
-static void *get_item_ptr(void_buff_t *self, size_t index, size_t item_size) {
-  return self->P_items + (self->P_first + index) % self->P_capacity * item_size;
+static void *get_item_ptr(void_buff_t *self, size_t index) {
+  return self->P_items +
+         (self->P_first + index) % self->P_capacity * self->P_item_size;
 }
 
-void P_nt_circ_buf_dyn_push(void *self, void const *item, size_t item_size) {
+void P_nt_circ_buf_dyn_push(void *self, void const *item) {
   void_buff_t *self2 = self;
 
   nt_assert(self2->P_count < self2->P_capacity,
             "Cannot push on a full circular buffer");
 
-  void *ptr = get_item_ptr(self2, self2->P_count, item_size);
-  memcpy(ptr, item, item_size);
+  void *ptr = get_item_ptr(self2, self2->P_count);
+  memcpy(ptr, item, self2->P_item_size);
 
   ++self2->P_count;
 }
 
-void P_nt_circ_buf_dyn_pop(void *self, void *item, size_t item_size) {
+void P_nt_circ_buf_dyn_pop(void *self, void *item) {
   void_buff_t *self2 = self;
 
   nt_assert(self2->P_count > 0, "Cannot pop from an empty circular buffer");
 
   if (item) {
-    void *ptr = get_item_ptr(self2, 0, item_size);
-    memcpy(item, ptr, item_size);
+    void *ptr = get_item_ptr(self2, 0);
+    memcpy(item, ptr, self2->P_item_size);
   }
 
   self2->P_first = (self2->P_first + 1) % self2->P_capacity;
   --self2->P_count;
 }
 
-void const *P_nt_circ_buf_dyn_get(void const *self, size_t index,
-                                  size_t item_size) {
+void const *P_nt_circ_buf_dyn_get(void const *self, size_t index) {
   void_buff_t const *self2 = self;
 
   nt_assert(
@@ -43,37 +43,35 @@ void const *P_nt_circ_buf_dyn_get(void const *self, size_t index,
       "Index %zu is out of range for a circular buffer containing %zu items",
       index, self2->P_count);
 
-  return get_item_ptr((void_buff_t *)self2, index, item_size);
+  return get_item_ptr((void_buff_t *)self2, index);
 }
 
 NT_CIRC_BUF(void_buff_fx, char, 1)
 
 static void *get_item_ptr_fx(void_buff_fx_t *self, size_t capacity,
-                             size_t index, size_t item_size) {
-  return self->P_items + (self->P_first + index) % capacity * item_size;
+                             size_t index) {
+  return self->P_items + (self->P_first + index) % capacity * self->P_item_size;
 }
 
-void P_nt_circ_buf_fx_push(void *self, size_t capacity, void const *item,
-                           size_t item_size) {
+void P_nt_circ_buf_fx_push(void *self, size_t capacity, void const *item) {
   void_buff_fx_t *self2 = self;
 
   nt_assert(self2->P_count < capacity, "Cannot push on a full circular buffer");
 
-  void *ptr = get_item_ptr_fx(self2, capacity, self2->P_count, item_size);
-  memcpy(ptr, item, item_size);
+  void *ptr = get_item_ptr_fx(self2, capacity, self2->P_count);
+  memcpy(ptr, item, self2->P_item_size);
 
   ++self2->P_count;
 }
 
-void P_nt_circ_buf_fx_pop(void *self, size_t capacity, void *item,
-                          size_t item_size) {
+void P_nt_circ_buf_fx_pop(void *self, size_t capacity, void *item) {
   void_buff_fx_t *self2 = self;
 
   nt_assert(self2->P_count > 0, "Cannot pop from an empty circular buffer");
 
   if (item) {
-    void *ptr = get_item_ptr_fx(self2, capacity, 0, item_size);
-    memcpy(item, ptr, item_size);
+    void *ptr = get_item_ptr_fx(self2, capacity, 0);
+    memcpy(item, ptr, self2->P_item_size);
   }
 
   self2->P_first = (self2->P_first + 1) % capacity;
@@ -81,7 +79,7 @@ void P_nt_circ_buf_fx_pop(void *self, size_t capacity, void *item,
 }
 
 void const *P_nt_circ_buf_fx_get(void const *self, size_t capacity,
-                                 size_t index, size_t item_size) {
+                                 size_t index) {
   void_buff_fx_t const *self2 = self;
 
   nt_assert(
@@ -89,5 +87,5 @@ void const *P_nt_circ_buf_fx_get(void const *self, size_t capacity,
       "Index %zu is out of range for a circular buffer containing %zu items",
       index, self2->P_count);
 
-  return get_item_ptr_fx((void_buff_fx_t *)self2, capacity, index, item_size);
+  return get_item_ptr_fx((void_buff_fx_t *)self2, capacity, index);
 }
--- source/list.c
+++ source/list.c
@@ -20,7 +20,7 @@ static size_t align_capacity(size_t n) {
   return 1ull << (64u - __builtin_clzll(n - 1));
 }
 
-void P_nt_list_reserve(void *self, size_t amount, size_t item_size) {
+void P_nt_list_reserve(void *self, size_t amount) {
   void_list_t *self2 = self;
 
   size_t new_capacity = self2->P_count + amount;
@@ -29,7 +29,7 @@ void P_nt_list_reserve(void *self, size_t amount, size_t item_size) {
   }
 
   new_capacity = align_capacity(new_capacity);
-  size_t new_capacity_bytes = new_capacity * item_size;
+  size_t new_capacity_bytes = new_capacity * self2->P_item_size;
 
   if (self2->P_capacity == 0) {
     self2->P_items = nt_alloc(char, new_capacity_bytes);
@@ -40,7 +40,7 @@ void P_nt_list_reserve(void *self, size_t amount, size_t item_size) {
   self2->P_capacity = new_capacity;
 }
 
-void P_nt_list_shrink(void *self, size_t item_size) {
+void P_nt_list_shrink(void *self) {
   void_list_t *self2 = self;
 
   if (self2->P_capacity == self2->P_count) {
@@ -51,56 +51,55 @@ void P_nt_list_shrink(void *self, size_t item_size) {
     nt_free(self2->P_items);
     self2->P_items = NULL;
   } else {
-    size_t new_capacity_bytes = self2->P_count * item_size;
+    size_t new_capacity_bytes = self2->P_count * self2->P_item_size;
     self2->P_items = nt_realloc(self2->P_items, new_capacity_bytes);
   }
 
   self2->P_capacity = self2->P_count;
 }
 
-size_t P_nt_list_insert(void *self, size_t index, void const *item,
-                        size_t item_size) {
+size_t P_nt_list_insert(void *self, size_t index, void const *item) {
   void_list_t *self2 = self;
   nt_assert(index <= self2->P_count,
             "Invalid insertion index %zu for list containing %zu items", index,
             self2->P_count);
 
   if (self2->P_count == self2->P_capacity) {
-    P_nt_list_reserve(self, 1, item_size);
+    P_nt_list_reserve(self, 1);
   }
 
   if (index < self2->P_count) {
-    memmove(&self2->P_items[(index + 1) * item_size],
-            &self2->P_items[index * item_size],
-            (self2->P_count - index) * item_size);
+    memmove(&self2->P_items[(index + 1) * self2->P_item_size],
+            &self2->P_items[index * self2->P_item_size],
+            (self2->P_count - index) * self2->P_item_size);
   }
 
-  memcpy(&self2->P_items[index * item_size], item, item_size);
+  memcpy(&self2->P_items[index * self2->P_item_size], item, self2->P_item_size);
 
   ++self2->P_count;
   return index;
 }
 
-void P_nt_list_remove(void *self, size_t index, size_t item_size) {
+void P_nt_list_remove(void *self, size_t index) {
   void_list_t *self2 = self;
   nt_assert(index < self2->P_count,
             "Invalid index %zu for list containing %zu items", index,
             self2->P_count);
 
   if (index < self2->P_count - 1) {
-    memmove(&self2->P_items[index * item_size],
-            &self2->P_items[(index + 1) * item_size],
-            (self2->P_count - index - 1) * item_size);
+    memmove(&self2->P_items[index * self2->P_item_size],
+            &self2->P_items[(index + 1) * self2->P_item_size],
+            (self2->P_count - index - 1) * self2->P_item_size);
   }
 
   --self2->P_count;
 }
 
-void const *P_nt_list_get(void const *self, size_t index, size_t item_size) {
+void const *P_nt_list_get(void const *self, size_t index) {
   void_list_t const *self2 = self;
   nt_assert(index < self2->P_count,
             "Invalid index %zu for list containing %zu items", index,
             self2->P_count);
 
-  return &self2->P_items[index * item_size];
+  return &self2->P_items[index * self2->P_item_size];
 }
