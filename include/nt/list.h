4af3f24adb295d215a1a82af831e51d4a6dcec01
Tobias Erbshäußer <tobias.erbshaeusser@gmail.com>
2020-08-04 07:52:23 +0200

    Moved item size for circular buffers and lists into the struct.

--- include/nt/list.h
+++ include/nt/list.h
@@ -8,10 +8,13 @@
   typedef struct {                                                             \
     size_t P_capacity;                                                         \
     size_t P_count;                                                            \
+    size_t P_item_size;                                                        \
     item_type *P_items;                                                        \
   } name##_t;                                                                  \
                                                                                \
-  static inline name##_t name##_new() { return (name##_t){0, 0, NULL}; }       \
+  static inline name##_t name##_new() {                                        \
+    return (name##_t){0, 0, sizeof(item_type), NULL};                          \
+  }                                                                            \
                                                                                \
   static inline void name##_free(name##_t *self) { nt_free(self->P_items); }   \
                                                                                \
@@ -24,51 +27,48 @@
   }                                                                            \
                                                                                \
   static inline void name##_reserve(name##_t *self, size_t amount) {           \
-    P_nt_list_reserve(self, amount, sizeof(item_type));                        \
+    P_nt_list_reserve(self, amount);                                           \
   }                                                                            \
                                                                                \
-  static inline void name##_shrink(name##_t *self) {                           \
-    P_nt_list_shrink(self, sizeof(item_type));                                 \
-  }                                                                            \
+  static inline void name##_shrink(name##_t *self) { P_nt_list_shrink(self); } \
                                                                                \
   static inline size_t name##_add(name##_t *self, item_type item) {            \
-    return P_nt_list_insert(self, self->P_count, &item, sizeof(item_type));    \
+    return P_nt_list_insert(self, self->P_count, &item);                       \
   }                                                                            \
                                                                                \
   static inline size_t name##_add_ref(name##_t *self, item_type const *item) { \
-    return P_nt_list_insert(self, self->P_count, item, sizeof(item_type));     \
+    return P_nt_list_insert(self, self->P_count, item);                        \
   }                                                                            \
                                                                                \
   static inline void name##_insert(name##_t *self, size_t index,               \
                                    item_type item) {                           \
-    P_nt_list_insert(self, index, &item, sizeof(item_type));                   \
+    P_nt_list_insert(self, index, &item);                                      \
   }                                                                            \
                                                                                \
   static inline void name##_insert_ref(name##_t *self, size_t index,           \
                                        item_type const *item) {                \
-    P_nt_list_insert(self, index, item, sizeof(item_type));                    \
+    P_nt_list_insert(self, index, item);                                       \
   }                                                                            \
                                                                                \
   static inline void name##_remove(name##_t *self, size_t index) {             \
-    P_nt_list_remove(self, index, sizeof(item_type));                          \
+    P_nt_list_remove(self, index);                                             \
   }                                                                            \
                                                                                \
   static inline void name##_clear(name##_t *self) { self->P_count = 0; }       \
                                                                                \
   static inline item_type const *name##_get(name##_t const *self,              \
                                             size_t index) {                    \
-    return P_nt_list_get(self, index, sizeof(item_type));                      \
+    return P_nt_list_get(self, index);                                         \
   }
 
-void P_nt_list_reserve(void *self, size_t amount, size_t item_size);
+void P_nt_list_reserve(void *self, size_t amount);
 
-void P_nt_list_shrink(void *self, size_t item_size);
+void P_nt_list_shrink(void *self);
 
-size_t P_nt_list_insert(void *self, size_t index, void const *item,
-                        size_t item_size);
+size_t P_nt_list_insert(void *self, size_t index, void const *item);
 
-void P_nt_list_remove(void *self, size_t index, size_t item_size);
+void P_nt_list_remove(void *self, size_t index);
 
-void const *P_nt_list_get(void const *self, size_t index, size_t item_size);
+void const *P_nt_list_get(void const *self, size_t index);
 
 #endif // NTUTILS_LIST_H
